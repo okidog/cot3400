a659aac5d1afc65961e1713af29d71a6933df1c8
Oki <gtracer465@gmail.com>
2023-09-12 22:46:35 -0400

    Insertion sort, broken clock

--- asgt1/asgt1.cpp
+++ asgt1/asgt1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 const int LENGTH = 100; // Const int to change list length
@@ -17,8 +18,31 @@ void printList() {
     }
 }
 
+void insertionSort() {
+    int temp = 0;
+    int tmpIndex = 0;
+    float timeStarted = clock();
+    for (int i = 1; i < LENGTH; i++) {
+        if (list[i] < list[i-1]) {
+            tmpIndex = i;
+            while (list[tmpIndex] < list[tmpIndex-1]) {
+                temp = list[tmpIndex-1];
+                list[tmpIndex-1] = list[tmpIndex];
+                list[tmpIndex] = temp;
+                tmpIndex--;
+            }
+        }
+    }
+    float timeElapsed = clock() - timeStarted;
+    cout << endl << "This operation took " << timeElapsed << " cycles, or " << timeElapsed / CLOCKS_PER_SEC << " sec." << endl;
+}
+
 int main() {
+    srand(time(0));
     generateList();
+    printList();
+    cout << endl;
+    insertionSort();
     printList();
     return 0;
 };
